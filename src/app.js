3c4083f949069f185a6ed16472fef61bf572ada7
Yurii Kosynets <ykosinets@gmail.com>
2021-08-19 00:19:29 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+let options = {
+    className: {
+        pageScrolled: "page--scrolled",
+        pageLoaded: "page--loaded",
+        headerMenuToggleActive: 'header__menu-toggle--active'
+    },
+    selector: {
+        page: '.page',
+        header: '.header',
+        headerMenuToggle: '.header__menu-toggle'
+    }
+}
+
+window.addEventListener('load', () => {
+    let page = document.querySelector(options.selector.page);
+
+    page.classList.add(options.className.pageLoaded);
+});
+
+window.addEventListener('scroll', () => {
+    let page = document.querySelector(options.selector.page);
+    let header = document.querySelector(options.selector.header);
+    let headerHeight = header.getBoundingClientRect().height;
+
+    if (window.scrollY > (headerHeight + 10)) {
+        page.classList.add(options.className.pageScrolled);
+    } else {
+        if (page.classList.contains(options.className.pageScrolled)) page.classList.remove(options.className.pageScrolled);
+    }
+});
+
+let headerMenuToggle = document.querySelector(options.selector.headerMenuToggle);
+headerMenuToggle.addEventListener('click', () => {
+    headerMenuToggle.classList.toggle(options.className.headerMenuToggleActive);
+});
